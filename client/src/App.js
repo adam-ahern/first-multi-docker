b7ba3984e730a70dd31fab49e9563db07929408e
noriom42 <adamahern2312@gmail.com>
2021-09-21 21:23:08 +0100

    Testing AWS deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Pushed to AWS and working!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
